98ab86dec73aa07259a08afda739ba2910e00924
JaniLei <janileinonen12@gmail.com>
2016-11-17 13:38:34 +0200

    player script nimimuutos

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,36 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMove : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public float xSpeed = 0f;
     public float ySpeed = 0f;
     private float playerX = 0f;
     private float playerY = 0f;
     public Rigidbody2D rb;
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-	    if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             playerX = -1;
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             playerX = 1;
         }
-        else 
+        else
         {
             playerX = 0;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(transform.up * ySpeed);
         }
@@ -41,5 +42,12 @@ public class PlayerMove : MonoBehaviour {
 
         transform.Translate(new Vector2(playerX * xSpeed, playerY * ySpeed), Space.World);
 
-	}
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
 }
