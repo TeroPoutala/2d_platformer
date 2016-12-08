135e1261c6128931cbbeaf4f3e52a1307f921f8d
JaniLei <janileinonen12@gmail.com>
2016-12-08 11:08:12 +0200

    camerastuff

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,16 +5,27 @@ public class CameraController : MonoBehaviour {
 
     public GameObject player;
 
-    private Vector3 offset;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
 
+    private Vector3 offset;
 
 	void Start ()
     {
         offset = transform.position - player.transform.position;
+        transform.position = player.transform.position + offset;
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(transform.position, player.transform.position);
 	}
 	
 	void LateUpdate () 
     {
-        transform.position = player.transform.position + offset;
+
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(transform.position, player.transform.position, fracJourney);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
 	}
 }
